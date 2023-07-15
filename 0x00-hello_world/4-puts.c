210bac4056c58c449dac47ca25fbbc7628498870
Abahi99 <bahiayoub021@gmail.com>
2023-07-15 14:49:08 -0700

    task3

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: String with the base of puts function
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
