8a9d52e1be7da5141a261671bcb2a97fd499c86a
Abahi99 <bahiayoub021@gmail.com>
2023-07-17 13:35:16 -0700

    task6

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int lowercase;
+
+	for (lowercase = 'z'; lowercase >= 'a'; lowercase--)
+		putchar(lowercase);
+	putchar("\n");
+
+
+
+
+
+
+	return (0);
+}
