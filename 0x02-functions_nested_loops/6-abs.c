7e6f74559e92c4bddee62bb157ea46aa4a4f7ee7
Abahi99 <bahiayoub021@gmail.com>
2023-07-19 13:01:39 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - check the code
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int)
+{
+
+	if (c < 0)
+	{
+		int abs_val;
+
+		abs_val = c * -1;
+		return (abs_val);
+	}
+	return (C);
+}
