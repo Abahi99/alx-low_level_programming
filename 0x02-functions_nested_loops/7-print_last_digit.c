bcd0278637a40f94afa0341ee5c556493088ce29
Abahi99 <bahiayoub021@gmail.com>
2023-07-19 13:11:12 -0700

    task8

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code
+ * @c: checked
+ * Return: Always 0.
+ */
+
+int print_last_digit(int c)
+{
+
+	int last;
+
+	last = c % 10;
+	
+	if (last < 0)
+	{
+
+		last = last * (-1);
+	}
+	_putchar(last);
+	return (last);
+}
