8f2ef847b13356637840098ef6815b1881748569
Abahi99 <bahiayoub021@gmail.com>
2023-07-19 09:55:24 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * 
+ * main -Entry point of the code
+ *
+ * success : Always 0
+ *
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a';ch <= 'z'; ch++)
+		_putchar(ch);
+
+	_putchar('\n');
+	return (0);
+}
