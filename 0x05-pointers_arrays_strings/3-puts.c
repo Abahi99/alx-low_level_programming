bcb357d23cbe7df944a7cacb10433c776c7c4bb9
Abahi99 <bahiayoub021@gmail.com>
2023-07-24 14:36:16 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
