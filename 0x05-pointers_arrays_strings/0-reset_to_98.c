024f139472cb4677e67901328b3e26a32fda4751
Abahi99 <bahiayoub021@gmail.com>
2023-07-24 14:21:15 -0700

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - take a pointer to a an int variables to 98
+ * @n; int parameter 
+ * Return: Nothings
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
