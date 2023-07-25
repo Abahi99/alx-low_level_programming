fb650f32e8f6d2c9cec6e1355f49d038dc106596
Abahi99 <bahiayoub021@gmail.com>
2023-07-25 09:31:53 -0700

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - prints odd num
+ * @str: checked
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int i, j, n;
+
+	n = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		n++;
+
+	j = n / 2;
+	if (n % 2 == 1)
+		j = (n + 1) / 2;
+	for (i = j; str[i] != '\0'; i ++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
