65becfd8cdc2c420c04e581d4dd9b7ba5b6d346a
Abahi99 <bahiayoub021@gmail.com>
2023-07-25 10:08:49 -0700

    task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcpy - check the code
+ * @dest: first parameter
+ * @src: second parameter
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int = -1;
+
+	do
+	{
+		i++;
+		dest[i] = src[i];
+	}while (src[i] != '\0');
+	return (dest);
+}
