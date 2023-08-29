57ffbad9e51a0233b425da06f9c59f47a683e00b
Abahi99 <bahiayoub021@gmail.com>
2023-08-29 11:45:14 -0700

    task7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp;
 
 	i = 0;
-	*temp = head;
+	temp = head;
 	while (temp && i < index)
 	{
 		temp = temp->next;
